a208d26ffa00b946762f59fc04bcaa78bb5e258b
jiangwolong2016 <wolong.jiang@b-and-qchian.com>
2016-09-08 17:43:42 +0800

    init ios and android moduls

---
+++ ios/RCTBaiduLocation/RCTBaiduLocation.h
@@ -0,0 +1,13 @@
+//
+//  RCTBaiduLocation.h
+//  RCTBaiduLocation
+//
+//  Created by user on 16/9/8.
+//  Copyright © 2016年 baj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTBaiduLocation : NSObject
+
+@end
---
+++ ios/RCTBaiduLocation/RCTBaiduLocation.m
@@ -0,0 +1,13 @@
+//
+//  RCTBaiduLocation.m
+//  RCTBaiduLocation
+//
+//  Created by user on 16/9/8.
+//  Copyright © 2016年 baj. All rights reserved.
+//
+
+#import "RCTBaiduLocation.h"
+
+@implementation RCTBaiduLocation
+
+@end
